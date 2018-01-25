df621bdfa8d8a77287157ddca33e6d8321588ef5
Tai Vu <tai.vu@cyberwoven.com>
2018-01-25 16:05:04 -0500

    delete public folder on each rebuild

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -40,11 +40,11 @@ gulp.task('init-watch', () => {
 })
 
 gulp.task('build', () => {
-    runSequence(['sass', 'js', 'fonts', 'images'], 'hugo')
+    runSequence(['sass', 'js', 'fonts', 'images', 'delete-pub'], 'hugo')
 })
 
 gulp.task('build-preview', () => {
-    runSequence(['sass', 'js', 'fonts', 'images'], 'hugo-preview')
+    runSequence(['sass', 'js', 'fonts', 'images', 'delete-pub'], 'hugo-preview')
 })
 
 gulp.task('hugo', (cb) => {
@@ -112,4 +112,13 @@ gulp.task('images', () => {
 
 gulp.task('cms-delete', () => {
     return del(['static/admin'], { dot: true })
+})
+
+gulp.task('delete-pub', () => {
+  return del(['public/**', '!public'], {
+    // dryRun: true,
+    dot: true
+  }).then(paths => {
+    console.log('Files and folders deleted:\n', paths.join('\n'), '\nTotal Files Deleted: ' + paths.length + '\n');
+  })
 })
